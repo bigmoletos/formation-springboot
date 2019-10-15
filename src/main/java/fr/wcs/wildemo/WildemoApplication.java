b626d867ebc07edef476fd090cc92464870bf08d
Jérémy Masson <jeremy.masson@wildcodeschool.fr>
2019-10-15 16:48:24 +0200

    Initiation projet.

---
+++ src/main/java/fr/wcs/wildemo/WildemoApplication.java
@@ -0,0 +1,13 @@
+package fr.wcs.wildemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildemoApplication.class, args);
+	}
+
+}
