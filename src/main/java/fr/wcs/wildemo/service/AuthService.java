c425c4477828e7ec5042aa1776c53fea97626500
bigmoletos <bigmoletos@yopmail.com>
2019-11-02 19:49:19 +0100

    modif suite à upstream merge master from le repo original de  jeremy

--- src/main/java/fr/wcs/wildemo/service/AuthService.java
+++ src/main/java/fr/wcs/wildemo/service/AuthService.java
@@ -2,10 +2,6 @@ package fr.wcs.wildemo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import fr.wcs.wildemo.repository.AccountRepository;
@@ -15,15 +11,14 @@ public class AuthService implements UserDetailsService {
 
 	@Autowired
 	private AccountRepository repo;
-	
+
 	@Override
-	public UserDetails loadUserByUsername(String username)
-			throws UsernameNotFoundException {
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		return this.repo.findOneByUsername(username);
 	}
-	
+
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
-	    return new BCryptPasswordEncoder();
+		return new BCryptPasswordEncoder();
 	}
 }
